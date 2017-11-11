5c28eafdfd1859a613ba4024d8cf9f5ed4a2c982
Alexander <asozinov@mera.ru>
2017-11-11 18:15:46 +0300

    Clear code; Readme file

---
+++ README.md
@@ -0,0 +1,8 @@
+# React JS test project
+
+**Run project:**
+
+1) npm install
+2) Install `react-scripts` if needed
+3) Change url server path and port (`src/common/utils.js`)
+3) Start project `npm run start` and go to `http://localhost:3000`
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import {App} from './components'
 import reducers from './reducers'
 import logger from 'redux-logger'
